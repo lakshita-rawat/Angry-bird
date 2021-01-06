d2ccf743793c9e88ebdb7b42f476610b3a261af5
lakshita-rawat <lakshitarawat2180@gmail.com>
2021-01-06 16:49:54 +0530

    Catapult and rubberband

--- Slingshot.js
+++ Slingshot.js
@@ -8,14 +8,33 @@ class SlingShot{
         }
         this.pointB=pointB;
         this.sling= Constraint.create(options);
+        this.sling1= loadImage("sprites/sling1.png");
+        this.sling2= loadImage("sprites/sling2.png");
+        this.sling3= loadImage("sprites/sling3.png");
         World.add(world,this.sling);
     }
     display(){
+        image(this.sling1,200,22);
+        image(this.sling2,170,22);
+        
         if(this.sling.bodyA){
             var pointA=this.sling.bodyA.position;
             var pointB=this.pointB;
-            strokeWeight(3);
-            line(pointA.x,pointA.y,pointB.x,pointB.y);
+            push();
+            stroke(48,22,8  );
+            if(pointA.x <220){
+                strokeWeight(7);
+                line(pointA.x-20,pointA.y,pointB.x-12,pointB.y+5);
+                line(pointA.x-20,pointA.y,pointB.x+30,pointB.y+5);
+                image(this.sling3,pointA.x-30,pointA.y-10,15,30);
+            }
+            else{
+                strokeWeight(3);
+                line(pointA.x+25,pointA.y,pointB.x-12,pointB.y+5);
+                line(pointA.x+25,pointA.y,pointB.x+30,pointB.y+5);
+                image(this.sling3,pointA.x+25,pointA.y-10,15,30);
+            }
+            pop();
         }
     }
     fly(){
